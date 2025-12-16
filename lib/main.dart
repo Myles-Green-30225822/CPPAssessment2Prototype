6d7c0e2bffbbdde4752f4d57420fd858fb03d2c3
Myles-Green-30225822 <30225822@blackpool.ac.uk>
2025-12-16 12:38:32 +0000

    Update main.dart to add a function for taking player input based on which button they press
    The player can now press either rock, paper or scissors buttons, which will give a variable the value of one of the three mentioned buttons, which can then be used for future reference.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,15 +30,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,37 +37,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _result = "";
+  String _AIresult = "";
 
-  void _incrementCounter() {
+  void _winningPlayer(String _result) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      print(_result);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: 
       Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -85,24 +61,24 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_AIresult',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 FloatingActionButton(
-                  onPressed: _incrementCounter,
+                  onPressed: () => _winningPlayer("Rock"),
                   tooltip: 'Rock',
                   child: const Icon(Icons.handshake),
                 ),
                 FloatingActionButton(
-                  onPressed: _incrementCounter,
+                  onPressed: () => _winningPlayer("Paper"),
                   tooltip: 'Paper',
                   child: const Icon(Icons.copy),
                 ),
                 FloatingActionButton(
-                  onPressed: _incrementCounter,
+                  onPressed: () => _winningPlayer("Scissors"),
                   tooltip: 'Scissors',
                   child: const Icon(Icons.cut),
                 )
