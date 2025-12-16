3dad66ee37ac074fd8ab5c3d8a11f1bf617db775
Myles-Green-30225822 <30225822@blackpool.ac.uk>
2025-12-16 10:57:17 +0000

    Update main.dart, adding a third button
    A third button was added to the project in preparation to create a rock, paper, scissors game

--- lib/main.dart
+++ lib/main.dart
@@ -103,16 +103,20 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Row(children: [FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.handshake),
-      ),
-       FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-       // This trailing comma makes auto-formatting nicer for build methods.
-    ),],)
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.handshake),
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.copy),
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.cut),
+            ),],)
           ],
         ),
       ),
